2db602272a3b7a4e9344c1c7f0ccbe810ffc726e
WmS0ng <15830963896@163.com>
2023-08-23 15:39:51 +0800

    添加二维码生成配置类

--- src/main/java/com/example/warehouse/WarehouseApplication.java
+++ src/main/java/com/example/warehouse/WarehouseApplication.java
@@ -1,8 +1,11 @@
 package com.example.warehouse;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// mapper结构扫描器，指明mapper接口所在包，然后就会自动为mapper接口创建代理对象并加入到IOC容器
+@MapperScan(basePackages = "com.example.warehouse.mapper")
 @SpringBootApplication
 public class WarehouseApplication {
 
