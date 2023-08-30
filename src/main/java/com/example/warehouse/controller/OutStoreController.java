0955ed24121f97bb1de64534b004798fcaae2ac6
WmS0ng <15830963896@163.com>
2023-08-30 16:26:25 +0800

    添加仓库列表查询功能

--- src/main/java/com/example/warehouse/controller/OutStoreController.java
+++ src/main/java/com/example/warehouse/controller/OutStoreController.java
@@ -2,8 +2,10 @@ package com.example.warehouse.controller;
 
 import com.example.warehouse.dto.CurrentUser;
 import com.example.warehouse.entity.OutStore;
+import com.example.warehouse.entity.Store;
 import com.example.warehouse.result.Result;
 import com.example.warehouse.service.OutStoreService;
+import com.example.warehouse.service.StoreService;
 import com.example.warehouse.utils.TokenUtils;
 import com.example.warehouse.utils.WarehouseConstants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("outstore")
 public class OutStoreController {
@@ -19,6 +23,8 @@ public class OutStoreController {
     private OutStoreService outStoreService;
     @Autowired
     private TokenUtils tokenUtils;
+    @Autowired
+    private StoreService storeService;
 
     /**
      * 添加出库单
@@ -28,5 +34,14 @@ public class OutStoreController {
         CurrentUser currentUser = tokenUtils.getCurrentUser(token);
         outStore.setCreateBy(currentUser.getUserId());
         return outStoreService.insert(outStore);
+    }
+
+    /**
+     * 查询所有仓库
+     */
+    @RequestMapping("/store-list")
+    public Result storeList() {
+        List<Store> storeList = storeService.selectList();
+        return Result.ok(storeList);
     }
 }
